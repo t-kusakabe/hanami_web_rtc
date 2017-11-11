1f1dfd14e09047f45705b56ddac18bf504d31642
t-kusakabe <ttmac0704@gmail.com>
2017-11-12 02:25:09 +0900

    Create script

---
+++ apps/web/assets/javascripts/script.js
@@ -0,0 +1,15 @@
+'use strict';
+console.log(211)
+
+let localStream = null;
+
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+    .then(function (stream) {
+        // Success
+        $('#my-video').get(0).srcObject = stream;
+        localStream = stream;
+    }).catch(function (error) {
+        // Error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
